ca4b238b148b1e0dd57c2ff4ca8f37f958b0b776
AdamLagerqvist <adam.lagerqvist@elev.ga.ntig.se>
2022-04-13 12:44:51 +0200

    updated api route

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../../database');
-/* 
-    BASE URL /tasks
-    GET / - Get all tasks
-    POST / - Create a new task
-    GET /:id - Get a task by id
-    PUT /:id - Update a task by id
-    DELETE /:id - Delete a task by id
-*/
+
 router.get('/', async (req, res, next) => {
     await pool.promise()
-        .query('SELECT * FROM tasks')
+        .query('SELECT * FROM admlat_tasks')
         .then(([rows, fields]) => {
             res.json({
                 tasks: {
